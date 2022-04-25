4c51ac9dee3c64a9af1eb46297111a294c45f08d
SecretStrawberry <surduhori@gmail.com>
2022-04-25 12:21:49 +0200

    test styles new item cont

--- src/books/BooksCarousel.js
+++ src/books/BooksCarousel.js
@@ -8,13 +8,13 @@ import CarouselItem from "./CarouselItem";
 const CarouselWrapper = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400&display=swap");
 
-  display: grid;
+  /* display: grid;
   grid-auto-flow: column;
   justify-content: start;
 
   grid-template-rows: 2rem 1fr;
   column-gap: 1rem;
-  row-gap: 1rem;
+  row-gap: 1rem; */
 
   background-color: ${colors.vanilla};
   box-shadow: 0 0 4px ${colors.grey};
@@ -27,18 +27,22 @@ const CarouselWrapper = styled.div`
 
   & > h2 {
     font-size: 1.6rem;
-    grid-column: 1 / 3;
-    grid-row: 1 / 1;
     font-family: "Josefin Sans", sans-serif;
     font-weight: 500;
   }
+`;
+
+const ItemsContainer = styled.div`
+  margin-top: 1rem;
 `;
 
 function BooksCarousel() {
   return (
     <CarouselWrapper>
       <h2>Currently reading</h2>
-      <CarouselItem />
+      <ItemsContainer>
+        <CarouselItem />
+      </ItemsContainer>
     </CarouselWrapper>
   );
 }
